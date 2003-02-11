6a1e638f99d3036661a2c0e70e9f04725ed5c9b2
Geunyoung Park <gypark@gmail.com>
2003-02-11 09:02:25 +0000

    최근변경내역 목록에서 새로 생긴 페이지에 "(NEW)" 표시

--- wiki.pl
+++ wiki.pl
@@ -33,7 +33,7 @@ use strict;
 ### added by gypark
 ### wiki.pl 버전 정보
 use vars qw($WikiVersion $WikiRelease $HashKey);
-$WikiVersion = "0.92K3-ext1.20";
+$WikiVersion = "0.92K3-ext1.21";
 $WikiRelease = "2003-02-11";
 
 $HashKey = "salt"; # 2-character string
@@ -856,6 +856,13 @@ sub GetRcHtml {
 				$count .= $tChanges;
 			}
 			$count .= ") ";
+###############
+### added by gypark
+### 새 글 옆에는 New 마크 표시
+		} else {
+			$count = "<font color=red>(".&T('NEW').")</font>";
+###
+###############
 		}
 		$link = "";
 		if ($UseDiff && &GetParam("diffrclink", 1)) {
