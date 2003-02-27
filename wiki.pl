512a393e75bb325b2a86aab52b4e2e970850f9ec
Geunyoung Park <gypark@gmail.com>
2003-02-27 10:17:51 +0000

    goto 매크로 버그 수정

--- wiki.pl
+++ wiki.pl
@@ -33,8 +33,8 @@ use strict;
 ### added by gypark
 ### wiki.pl 버전 정보
 use vars qw($WikiVersion $WikiRelease $HashKey);
-$WikiVersion = "0.92K3-ext1.29";
-$WikiRelease = "2003-02-26";
+$WikiVersion = "0.92K3-ext1.29a";
+$WikiRelease = "2003-02-27";
 
 $HashKey = "salt"; # 2-character string
 ###
@@ -2652,6 +2652,13 @@ sub MacroHistory {
 
 sub MacroGoto {
 	my ($string) = @_;
+
+###############
+### added by gypark
+### goto 매크로 개선
+	$string = &RemoveLink($string);
+###
+###############
 
 	return
 ###############
