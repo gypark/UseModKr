3cb26d30596d8876f93ad5f16209b2570294ad45
Geunyoung Park <gypark@gmail.com>
2004-05-21 01:03:02 +0000

    달력에서 페이지 이름에 해당하는 날짜를 별도로 표시

--- wiki.pl
+++ wiki.pl
@@ -33,8 +33,8 @@ use strict;
 ### added by gypark
 ### wiki.pl 버전 정보
 use vars qw($WikiVersion $WikiRelease $HashKey);
-$WikiVersion = "0.92K3-ext1.60e";
-$WikiRelease = "2004-05-15";
+$WikiVersion = "0.92K3-ext1.60f";
+$WikiRelease = "2004-05-21";
 
 $HashKey = "salt"; # 2-character string
 ###
@@ -3444,6 +3444,9 @@ sub MacroCalendar {
 				$span_style = "text-decoration: underline; ";
 			} else {
 				$td_class = "calendar";
+			}
+			if ($pageid eq $cal_mainpage.$cal_page) {
+				$td_class .= "thispage";
 			}
 
 			if ((-f &GetPageFile($cal_mainpage . $cal_page)) && (!&PageIsHidden($cal_mainpage . $cal_page))) {
