cd2d223be753179a4d7292e3199382ce97760b4b
Geunyoung Park <gypark@gmail.com>
2005-02-26 06:40:33 +0000

    *** empty log message ***

--- wiki.pl
+++ wiki.pl
@@ -33,7 +33,7 @@ use strict;
 ### added by gypark
 ### wiki.pl 버전 정보
 use vars qw($WikiVersion $WikiRelease $HashKey);
-$WikiVersion = "0.92K3-ext1.74f";
+$WikiVersion = "0.92K3-ext1.74h";
 $WikiRelease = "2005-02-26";
 
 $HashKey = "salt"; # 2-character string
