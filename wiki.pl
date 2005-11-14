1eaf81a82bbde020cacdb5b6e13099518951a0bd
Geunyoung Park <gypark@gmail.com>
2005-11-14 03:06:28 +0000

    rss 에 나오는 IP주소의 마지막 자리 감춤

--- wiki.pl
+++ wiki.pl
@@ -33,8 +33,8 @@ use strict;
 ### added by gypark
 ### wiki.pl 버전 정보
 use vars qw($WikiVersion $WikiRelease $HashKey);
-$WikiVersion = "0.92K3-ext1.87";
-$WikiRelease = "2005-06-28";
+$WikiVersion = "0.92K3-ext1.88";
+$WikiRelease = "2005-11-14";
 
 $HashKey = "salt"; # 2-character string
 ###
@@ -8763,6 +8763,7 @@ sub GetRssRcLine {
 		$description = &QuoteHtml($summary);
 	}
 	$host = &QuoteHtml($host);
+	$host =~ s/\d+$/xxx/;
 	if ($userName) {
 		$author = &QuoteHtml($userName);
 		$authorLink = "link=\"$QuotedFullUrl?$author\"";
