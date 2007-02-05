5760ff5c3fe0e61dbaea2b4f1a929f2a1cb48d0c
Geunyoung Park <gypark@gmail.com>
2007-02-05 06:22:52 +0000

    섹션 단위 편집 - 섹션 제목에 따옴표가 있으면 summary 파라메터에는 &quot;으로 변환

--- wiki.pl
+++ wiki.pl
@@ -33,8 +33,8 @@ use strict;
 ### added by gypark
 ### wiki.pl 버전 정보
 use vars qw($WikiVersion $WikiRelease $HashKey);
-$WikiVersion = "0.92K3-ext1.106";
-$WikiRelease = "2007-02-04";
+$WikiVersion = "0.92K3-ext1.106a";
+$WikiRelease = "2007-02-05";
 
 $HashKey = "salt"; # 2-character string
 ###
@@ -5576,6 +5576,7 @@ sub DoEdit {
 				my $h_str = $1;
 				$h_str =~ /^[ \t]*\=+\s+(#\s+)?([^\n]+)\s+\=+\s*$/;
 				$h_str = $2;
+				$h_str =~ s/"/&quot;/g;
 				$q->param("summary", "$h_str - ".&GetParam("summary", ""));
 			}
 		}
