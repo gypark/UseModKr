11be7b9b2dd518cab4acad2cf971d8df1add4a09
Geunyoung Park <gypark@gmail.com>
2007-02-08 12:20:59 +0000

    Trackback - 주소 클립보드 복사 - 모질라 지원

--- wikiscript.js
+++ wikiscript.js
@@ -25,11 +25,54 @@ function chk_close(e, str) {
 	}
 }
 
-// 트랙백 주소 복사
-function copy_clip(msg, url) {
-	if (window.clipboardData) {
-		if (confirm(msg)) {
-			window.clipboardData.setData("Text", url);
-		}
+// msg - 사용자에게 확인창을 띄울때 출력되는 메시지
+// text - 클립보드에 복사될 텍스트
+// 출처: http://www.krikkit.net/howto_javascript_copy_clipboard.html
+// modified by raymundo, gypark@gmail.com
+
+// Copyright (C) krikkit - krikkit@gmx.net
+// --> http://www.krikkit.net
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+function copy_clip(msg, text) {
+	if (!confirm(msg)) return;
+
+	// IE
+	if (window.clipboardData) { 
+		window.clipboardData.setData("Text", text);
 	}
+	// Firefox/Mozilla
+	else if (window.netscape) {
+		// firefox/mozilla 에서 동작하기 위해서는 사용자 프로파일 디렉토리에 prefs.js 파일에 다음과 같이 적어준다
+		// user_pref("signed.applets.codebase_principal_support", true);
+		// 또는 "about:config" 페이지를 열어서 다음 항목의 값을 true로 설정해 준다
+		// signed.applets.codebase_principal_support
+
+		netscape.security.PrivilegeManager.enablePrivilege('UniversalXPConnect');
+
+		var clip = Components.classes['@mozilla.org/widget/clipboard;1'].createInstance(Components.interfaces.nsIClipboard);
+		if (!clip) return;
+
+		var trans = Components.classes['@mozilla.org/widget/transferable;1'].createInstance(Components.interfaces.nsITransferable);
+		if (!trans) return;
+
+		trans.addDataFlavor('text/unicode');
+
+		var str = Components.classes["@mozilla.org/supports-string;1"].createInstance(Components.interfaces.nsISupportsString);
+		var copytext=text;
+
+		str.data=copytext;
+
+		trans.setTransferData("text/unicode",str,copytext.length*2);
+
+		var clipid=Components.interfaces.nsIClipboard;
+
+		if (!clip) return false;
+
+		clip.setData(trans,null,clipid.kGlobalClipboard);
+	}
+	return false;
 }
+
