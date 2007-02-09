3ca8633ba1e8aaff629190ab13eb9d97d1355765
Geunyoung Park <gypark@gmail.com>
2007-02-09 08:13:21 +0000

    편집창 띄웠을 때 <!-- --> 안의 &가 &amp; 가 되는 문제 수정

--- wiki.pl
+++ wiki.pl
@@ -33,7 +33,7 @@ use strict;
 ### added by gypark
 ### wiki.pl 버전 정보
 use vars qw($WikiVersion $WikiRelease $HashKey);
-$WikiVersion = "0.92K3-ext1.109f";
+$WikiVersion = "0.92K3-ext1.110";
 $WikiRelease = "2007-02-09";
 
 $HashKey = "salt"; # 2-character string
