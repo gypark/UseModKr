9afba77344424569b8eac12349dbc5d66c9bfff4
Geunyoung Park <gypark@gmail.com>
2007-02-20 17:11:04 +0000

    adminonly 매크로 추가

---
+++ macros/adminonly.pl
@@ -0,0 +1,21 @@
+sub adminonly {
+	my ($txt) = @_;
+
+	$txt =~ s/&__LT__;adminonly&__GT__;(.*?)&__LT__;\/adminonly&__GT__;/&MacroAdminOnly($1)/geis;
+
+	return $txt;
+}
+
+sub MacroAdminOnly {
+	my ($blocktext) = @_;
+
+	$blocktext =~ s/^(\r?\n)(.*?)(\r?\n)$/$2/geis;
+
+	if (&UserIsAdmin()) {
+		return "$blocktext";
+	} else {
+		return "";
+	}
+}
+
+1;
