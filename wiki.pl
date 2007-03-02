ae2385ba7240bae83513ceee07ad74993f11d62c
Geunyoung Park <gypark@gmail.com>
2007-03-02 04:41:16 +0000

    *** empty log message ***

--- wiki.pl
+++ wiki.pl
@@ -32,8 +32,8 @@ use vars qw($ConfigFile $WikiVersion $WikiRelease $HashKey);
 ### 환경설정 파일의 경로
 $ConfigFile  = "config.pl";             # path of config file
 
-$WikiVersion = "0.92K3-ext2rc9";
-$WikiRelease = "2007-03-01";
+$WikiVersion = "0.92K3-ext2rc10";
+$WikiRelease = "2007-03-02";
 $HashKey = "salt"; # 2-character string
 
 local $| = 1;  # Do not buffer output (localized for mod_perl)
