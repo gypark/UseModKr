c06523e9d136a1fe525130046d2746819baf100e
Geunyoung Park <gypark@gmail.com>
2012-01-31 13:41:18 +0900

    version 2.18c

--- wiki.pl
+++ wiki.pl
@@ -33,7 +33,7 @@ use vars qw($ConfigFile $WikiVersion $WikiRelease $HashKey);
 ### 환경설정 파일의 경로
 $ConfigFile  = "config.pl";             # path of config file
 
-$WikiVersion = "0.92K3-ext2.18b";
+$WikiVersion = "0.92K3-ext2.18c";
 $WikiRelease = "2012-01-31";
 $HashKey = "salt"; # 2-character string
 
