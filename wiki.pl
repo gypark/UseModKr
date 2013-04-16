df528a7ac43567a73042cd0b227b3b43d84668df
Geunyoung Park <gypark@gmail.com>
2013-04-17 02:29:03 +0900

    wiki.css 에 글꼴 이름 오타 수정
    	modified:   wiki.css
    	modified:   wiki.pl

--- wiki.pl
+++ wiki.pl
@@ -33,8 +33,8 @@ use vars qw($ConfigFile $WikiVersion $WikiRelease $HashKey);
 ### 환경설정 파일의 경로
 $ConfigFile  = "config.pl";             # path of config file
 
-$WikiVersion = "0.92K3-ext2.26";
-$WikiRelease = "2012-11-25";
+$WikiVersion = "0.92K3-ext2.26a";
+$WikiRelease = "2013-04-17";
 $HashKey = "salt"; # 2-character string
 
 local $| = 1;  # Do not buffer output (localized for mod_perl)
