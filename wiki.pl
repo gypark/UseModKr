8ab606ff71285bc2bffb37c1272f150987c519b6
Geunyoung Park <gypark@gmail.com>
2024-05-15 22:51:22 +0900

    version 2.33

--- wiki.pl
+++ wiki.pl
@@ -34,8 +34,8 @@ use vars qw($ConfigFile $WikiVersion $WikiRelease $HashKey);
 ### 환경설정 파일의 경로
 $ConfigFile  = "./config.pl";             # path of config file
 
-$WikiVersion = "0.92K3-ext2.32";
-$WikiRelease = "2024-05-10";
+$WikiVersion = "0.92K3-ext2.33";
+$WikiRelease = "2024-05-15";
 $HashKey = "salt"; # 2-character string
 
 local $| = 1;  # Do not buffer output (localized for mod_perl)
