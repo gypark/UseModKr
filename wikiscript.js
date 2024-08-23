8f0eb9a998d261667455bfe2ac067db2a4fcadcb
Geunyoung Park <gypark@gmail.com>
2024-08-23 17:41:55 +0900

    wikiscript.js - 기존 onMemoToggle 함수 삭제

--- wikiscript.js
+++ wikiscript.js
@@ -1,17 +1,3 @@
-// 메모 매크로, 트랙백 등에 사용
-function onMemoToggle(id)
-{   
-    if (document.getElementById(id).style.display == "none")
-    {
-        document.getElementById(id).style.display = "block";
-    }
-    else
-    {
-        document.getElementById(id).style.display = "none";
-    }
-    return false;
-}
-
 // 작성 취소 시 확인
 var previous_text = "", current_text = "", conflict = false, closeok = false;
 function chk_close(e, str) {
